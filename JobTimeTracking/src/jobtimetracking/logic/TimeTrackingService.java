b004dccf91c9d63d1bbe7d0d8ac5e8fb0cb9bf16
aschmidt <anika.schmidt@xavo.com>
2019-05-29 20:42:28 +0200

    Editing different views parameters

---
+++ JobTimeTracking/src/jobtimetracking/logic/TimeTrackingService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobtimetracking.logic;
+
+/**
+ *
+ * @author Anika.Schmidt
+ */
+public class TimeTrackingService {
+    
+}
